d0e7dc8304010b80059a7858af838c66e55258ae
Pivopil <myworkplace8@gmail.com>
2021-02-07 20:33:17 +0200

    Added assume role

--- main.tf
+++ main.tf
@@ -1,12 +1,17 @@
 provider "aws" {
-}
-
-variable "bucket_name" {
-  type = string
-  description = "Bucket name"
-  default = "default-name"
+  assume_role {
+    role_arn = var.develop_assume_role
+  }
 }
 
 resource "aws_s3_bucket" "test-bucket" {
   bucket = var.bucket_name
+}
+
+variable "bucket_name" {
+  type = string
+}
+
+variable "develop_assume_role" {
+  type = string
 }

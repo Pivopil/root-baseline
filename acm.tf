35466e6c6bda029bb590743f9cc0dc77a6565d42
Pivopil <myworkplace8@gmail.com>
2021-02-08 21:43:25 +0200

    Added Acm module

---
+++ acm.tf
@@ -0,0 +1,25 @@
+data "aws_route53_zone" "public" {
+  name         = var.public_subdomain
+  private_zone = false
+}
+
+module "acm" {
+  source  = "terraform-aws-modules/acm/aws"
+  version = "2.12.0"
+
+  create_certificate = true
+  domain_name        = var.public_subdomain
+  zone_id            = data.aws_route53_zone.public.zone_id
+  subject_alternative_names = [
+    "api.${var.public_subdomain}",
+    "www.${var.public_subdomain}",
+    "app.${var.public_subdomain}",
+  ]
+  tags = {
+    Name = var.public_subdomain
+  }
+}
+
+variable "public_subdomain" {
+  default = ""
+}

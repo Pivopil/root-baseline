ddbdcc848449223fc85292e3277ceae62d3b9802
Pivopil <myworkplace8@gmail.com>
2021-02-08 22:19:54 +0200

    Added UA geolocation

--- cloud-front.tf
+++ cloud-front.tf
@@ -131,7 +131,7 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   restrictions {
     geo_restriction {
       restriction_type = "whitelist"
-      locations        = ["US", "CA", "GB", "DE"]
+      locations        = ["UA"]
     }
   }
 

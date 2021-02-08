a19a969e9eb97bf39fa7369bdf63086e86288c32
Pivopil <myworkplace8@gmail.com>
2021-02-08 22:10:55 +0200

    Try to fix bucket permissions

--- cloud-front.tf
+++ cloud-front.tf
@@ -39,7 +39,7 @@ resource "aws_s3_bucket" "s3_web_static_content_bucket" {
   bucket = local.web_static_content_bucket_name
 
   acl = "private"
-  policy = data.aws_iam_policy_document.s3_allow_gets.json
+//  policy = data.aws_iam_policy_document.s3_allow_gets.json
 
   force_destroy = true
 

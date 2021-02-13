8d65e3707c0b77e8a41fd4a17cfcf9a655a38a70
Pivopil <myworkplace8@gmail.com>
2021-02-13 18:06:57 +0200

    Fixed typo

--- ec2.tf
+++ ec2.tf
@@ -46,7 +46,7 @@ data "aws_iam_policy_document" "instance_assume_role_policy" {
     }
   }
   statement {
-    effect    = ""
+    effect    = "Allow"
     actions   = ["rds-db:connect"]
     resources = ["arn:aws:rds-db:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:*:db-*/*"]
   }

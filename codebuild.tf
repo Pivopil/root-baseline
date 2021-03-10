598a3cfe6b7ffb68f524cdf0aae933560cbea943
Pivopil <myworkplace8@gmail.com>
2021-03-10 19:08:14 +0200

    Removed code build and code pipeline

--- codebuild.tf
+++ codebuild.tf
@@ -1,125 +1,125 @@
-//https://github.com/aws-samples/aws-ecs-cicd-terraform/blob/master/terraform/codebuild.tf
-data "aws_iam_policy_document" "codebuild_role_document" {
-  version = "2012-10-17"
-  statement {
-    effect = "Allow"
-    actions = ["sts:AssumeRole"]
-    principals {
-      type        = "Service"
-      identifiers = ["codebuild.amazonaws.com"]
-    }
-  }
-}
-
-resource "aws_iam_role" "codebuild_role" {
-  name = "${var.prefix}-codebuild_role"
-  assume_role_policy = data.aws_iam_policy_document.codebuild_role_document.json
-}
-
-data "aws_iam_policy_document" "codebuild_policy_document" {
-  version = "2012-10-17"
-  statement {
-    actions   = ["logs:CreateLogGroup", "logs:CreateLogStream", "logs:PutLogEvents", "ecr:GetAuthorizationToken"]
-    effect    = "Allow"
-    resources = ["*"]
-  }
-  statement {
-    actions   = ["s3:GetObject", "s3:GetObjectVersion", "s3:PutObject"]
-    effect    = "Allow"
-    resources = ["${aws_s3_bucket.artifact_bucket.arn}/*"]
-  }
-  statement {
-    actions = ["ecr:GetDownloadUrlForLayer", "ecr:BatchGetImage",
-      "ecr:BatchCheckLayerAvailability", "ecr:PutImage",
-      "ecr:InitiateLayerUpload", "ecr:UploadLayerPart",
-    "ecr:CompleteLayerUpload"]
-    effect    = "Allow"
-    resources = [aws_ecr_repository.springboot_ecr_repository.arn]
-  }
-  statement {
-    actions = ["ecr:GetDownloadUrlForLayer",
-      "ecr:BatchGetImage",
-    "ecr:BatchCheckLayerAvailability"]
-    effect    = "Allow"
-    resources = [aws_ecr_repository.springboot_ecr_repository.arn]
-  }
-}
-
-resource "aws_iam_policy" "codebuild_policy" {
-  description = "Policy to allow codebuild to execute build spec"
-  policy      = data.aws_iam_policy_document.codebuild_policy_document.json
-}
-
-resource "aws_iam_role_policy_attachment" "codebuild-attach" {
-  role       = aws_iam_role.codebuild_role.name
-  policy_arn = aws_iam_policy.codebuild_policy.arn
-}
-
-resource "aws_codebuild_project" "codebuild" {
-  depends_on = [
-    aws_ecr_repository.springboot_ecr_repository
-  ]
-  name         = "${var.prefix}-codebuild"
-  service_role = aws_iam_role.codebuild_role.arn
-  artifacts {
-    type = "CODEPIPELINE"
-  }
-  environment {
-    compute_type                = "BUILD_GENERAL1_MEDIUM"
-    image                       = "aws/codebuild/standard:3.0"
-    type                        = "LINUX_CONTAINER"
-    privileged_mode             = true
-    image_pull_credentials_type = "CODEBUILD"
-    environment_variable {
-      name  = "REPOSITORY_URI"
-      value = aws_ecr_repository.springboot_ecr_repository.repository_url
-    }
-    environment_variable {
-      name  = "AWS_DEFAULT_REGION"
-      value = data.aws_region.current.id
-    }
-    environment_variable {
-      name  = "CONTAINER_NAME"
-      value = var.ecs_service_name
-    }
-  }
-  source {
-    type      = "CODEPIPELINE"
-    buildspec = <<BUILDSPEC
-version: 0.2
-env:
-  variables:
-    CODEBUILD_SRC_DIR: "/ecs-app"
-runtime-versions:
-  java: openjdk8
-phases:
-  install:
-    runtime-versions:
-      docker: 18
-  pre_build:
-    commands:
-      - echo Logging in to Amazon ECR...
-      - $(aws ecr get-login --region $AWS_DEFAULT_REGION --no-include-email)
-      - COMMIT_HASH=$(echo $CODEBUILD_RESOLVED_SOURCE_VERSION | cut -c 1-7)
-      - IMAGE_TAG=$${COMMIT_HASH:=latest}
-  build:
-    commands:
-      - echo Build started on `date`
-      - echo Building the jar
-      - find ./target/ -type f \( -name "*.jar" -not -name "*sources.jar" \) -exec cp {} ./$SERVICE_NAME.jar \;
-      - mvn clean install
-      - echo Building the Docker image...
-      - docker build -t $REPOSITORY_URI:latest .
-      - docker tag $REPOSITORY_URI:latest $REPOSITORY_URI:$IMAGE_TAG
-  post_build:
-    commands:
-      - echo Build completed on `date`
-      - echo Pushing the Docker image...
-      - docker push $REPOSITORY_URI:latest
-      - docker push $REPOSITORY_URI:$IMAGE_TAG
-      - printf '[{"name":"%s","image":"%s","essential":true,"environment":[{"name":"spring_profile_active","value":"default"}],"portMappings":[{"containerPort":8080}],"logConfiguration":{"logDriver":"awslogs","options":{"awslogs-group":"springbootapp-LogGroup","awslogs-region":"us-east-1","awslogs-stream-prefix":"springbootapp-LogGroup-stream"}}}]' $CONTAINER_NAME $REPOSITORY_URI:$IMAGE_TAG > imagedefinitions.json
-artifacts:
-    files: imagedefinitions.json
-BUILDSPEC
-  }
-}
+////https://github.com/aws-samples/aws-ecs-cicd-terraform/blob/master/terraform/codebuild.tf
+//data "aws_iam_policy_document" "codebuild_role_document" {
+//  version = "2012-10-17"
+//  statement {
+//    effect = "Allow"
+//    actions = ["sts:AssumeRole"]
+//    principals {
+//      type        = "Service"
+//      identifiers = ["codebuild.amazonaws.com"]
+//    }
+//  }
+//}
+//
+//resource "aws_iam_role" "codebuild_role" {
+//  name = "${var.prefix}-codebuild_role"
+//  assume_role_policy = data.aws_iam_policy_document.codebuild_role_document.json
+//}
+//
+//data "aws_iam_policy_document" "codebuild_policy_document" {
+//  version = "2012-10-17"
+//  statement {
+//    actions   = ["logs:CreateLogGroup", "logs:CreateLogStream", "logs:PutLogEvents", "ecr:GetAuthorizationToken"]
+//    effect    = "Allow"
+//    resources = ["*"]
+//  }
+//  statement {
+//    actions   = ["s3:GetObject", "s3:GetObjectVersion", "s3:PutObject"]
+//    effect    = "Allow"
+//    resources = ["${aws_s3_bucket.artifact_bucket.arn}/*"]
+//  }
+//  statement {
+//    actions = ["ecr:GetDownloadUrlForLayer", "ecr:BatchGetImage",
+//      "ecr:BatchCheckLayerAvailability", "ecr:PutImage",
+//      "ecr:InitiateLayerUpload", "ecr:UploadLayerPart",
+//    "ecr:CompleteLayerUpload"]
+//    effect    = "Allow"
+//    resources = [aws_ecr_repository.springboot_ecr_repository.arn]
+//  }
+//  statement {
+//    actions = ["ecr:GetDownloadUrlForLayer",
+//      "ecr:BatchGetImage",
+//    "ecr:BatchCheckLayerAvailability"]
+//    effect    = "Allow"
+//    resources = [aws_ecr_repository.springboot_ecr_repository.arn]
+//  }
+//}
+//
+//resource "aws_iam_policy" "codebuild_policy" {
+//  description = "Policy to allow codebuild to execute build spec"
+//  policy      = data.aws_iam_policy_document.codebuild_policy_document.json
+//}
+//
+//resource "aws_iam_role_policy_attachment" "codebuild-attach" {
+//  role       = aws_iam_role.codebuild_role.name
+//  policy_arn = aws_iam_policy.codebuild_policy.arn
+//}
+//
+//resource "aws_codebuild_project" "codebuild" {
+//  depends_on = [
+//    aws_ecr_repository.springboot_ecr_repository
+//  ]
+//  name         = "${var.prefix}-codebuild"
+//  service_role = aws_iam_role.codebuild_role.arn
+//  artifacts {
+//    type = "CODEPIPELINE"
+//  }
+//  environment {
+//    compute_type                = "BUILD_GENERAL1_MEDIUM"
+//    image                       = "aws/codebuild/standard:3.0"
+//    type                        = "LINUX_CONTAINER"
+//    privileged_mode             = true
+//    image_pull_credentials_type = "CODEBUILD"
+//    environment_variable {
+//      name  = "REPOSITORY_URI"
+//      value = aws_ecr_repository.springboot_ecr_repository.repository_url
+//    }
+//    environment_variable {
+//      name  = "AWS_DEFAULT_REGION"
+//      value = data.aws_region.current.id
+//    }
+//    environment_variable {
+//      name  = "CONTAINER_NAME"
+//      value = var.ecs_service_name
+//    }
+//  }
+//  source {
+//    type      = "CODEPIPELINE"
+//    buildspec = <<BUILDSPEC
+//version: 0.2
+//env:
+//  variables:
+//    CODEBUILD_SRC_DIR: "/ecs-app"
+//runtime-versions:
+//  java: openjdk8
+//phases:
+//  install:
+//    runtime-versions:
+//      docker: 18
+//  pre_build:
+//    commands:
+//      - echo Logging in to Amazon ECR...
+//      - $(aws ecr get-login --region $AWS_DEFAULT_REGION --no-include-email)
+//      - COMMIT_HASH=$(echo $CODEBUILD_RESOLVED_SOURCE_VERSION | cut -c 1-7)
+//      - IMAGE_TAG=$${COMMIT_HASH:=latest}
+//  build:
+//    commands:
+//      - echo Build started on `date`
+//      - echo Building the jar
+//      - find ./target/ -type f \( -name "*.jar" -not -name "*sources.jar" \) -exec cp {} ./$SERVICE_NAME.jar \;
+//      - mvn clean install
+//      - echo Building the Docker image...
+//      - docker build -t $REPOSITORY_URI:latest .
+//      - docker tag $REPOSITORY_URI:latest $REPOSITORY_URI:$IMAGE_TAG
+//  post_build:
+//    commands:
+//      - echo Build completed on `date`
+//      - echo Pushing the Docker image...
+//      - docker push $REPOSITORY_URI:latest
+//      - docker push $REPOSITORY_URI:$IMAGE_TAG
+//      - printf '[{"name":"%s","image":"%s","essential":true,"environment":[{"name":"spring_profile_active","value":"default"}],"portMappings":[{"containerPort":8080}],"logConfiguration":{"logDriver":"awslogs","options":{"awslogs-group":"springbootapp-LogGroup","awslogs-region":"us-east-1","awslogs-stream-prefix":"springbootapp-LogGroup-stream"}}}]' $CONTAINER_NAME $REPOSITORY_URI:$IMAGE_TAG > imagedefinitions.json
+//artifacts:
+//    files: imagedefinitions.json
+//BUILDSPEC
+//  }
+//}
--- codepipeline.tf
+++ codepipeline.tf
@@ -1,177 +1,177 @@
-//https://github.com/aws-samples/aws-ecs-cicd-terraform/blob/master/terraform/codepipeline.tf
-provider "github" {}
-
-data "aws_iam_policy_document" "codepipeline_role_document" {
-  version = "2012-10-17"
-  statement {
-    effect = "Allow"
-    actions = ["sts:AssumeRole"]
-    principals {
-      type        = "Service"
-      identifiers = ["codepipeline.amazonaws.com"]
-    }
-  }
-}
-
-resource "aws_iam_role" "codepipeline_role" {
-  name = "${var.prefix}-codepipeline_role"
-  assume_role_policy = data.aws_iam_policy_document.codepipeline_role_document.json
-}
-
-data "aws_iam_policy_document" "codepipeline_policy_document" {
-  version = "2012-10-17"
-  statement {
-    actions = ["s3:GetObject", "s3:GetObjectVersion", "s3:PutObject",
-    "s3:GetBucketVersioning"]
-    effect    = "Allow"
-    resources = ["${aws_s3_bucket.artifact_bucket.arn}/*"]
-  }
-  statement {
-    actions = ["codebuild:StartBuild", "codebuild:BatchGetBuilds",
-      "cloudformation:*",
-    "iam:PassRole"]
-    effect    = "Allow"
-    resources = ["*"]
-  }
-  statement {
-    actions   = ["ecs:*"]
-    effect    = "Allow"
-    resources = ["*"]
-  }
-}
-
-resource "aws_iam_policy" "codepipeline_policy" {
-  description = "Policy to allow codepipeline to execute"
-  policy      = data.aws_iam_policy_document.codepipeline_role_document.json
-}
-
-resource "aws_iam_role_policy_attachment" "codepipeline-attach" {
-  role       = aws_iam_role.codepipeline_role.name
-  policy_arn = aws_iam_policy.codepipeline_policy.arn
-}
-
-resource "aws_s3_bucket" "artifact_bucket" {
-}
-
-resource "aws_codepipeline" "pipeline" {
-  depends_on = [
-    aws_codebuild_project.codebuild,
-  ]
-  name     = "${var.prefix}-Pipeline"
-  role_arn = aws_iam_role.codepipeline_role.arn
-  artifact_store {
-    location = aws_s3_bucket.artifact_bucket.bucket
-    type     = "S3"
-  }
-  //  https://docs.aws.amazon.com/code-samples/latest/catalog/cloudformation-codepipeline-template-codepipeline-github-events-yaml.yml.html
-
-  stage {
-    name = "Source"
-    action {
-      name             = "Source"
-      category         = "Source"
-      owner            = "ThirdParty"
-      version          = "1"
-      provider         = "GitHub"
-      output_artifacts = ["SourceOutput"] # code
-      run_order        = 1
-      configuration = {
-        Owner                = var.repo_owner
-        Repo                 = var.repo_name
-        Branch               = var.branch
-        OAuthToken           = var.github_oauth_token
-        PollForSourceChanges = "false"
-      }
-    }
-  }
-
-  stage {
-    name = "Build"
-    action {
-      name             = "Build"
-      category         = "Build"
-      owner            = "AWS"
-      version          = "1"
-      provider         = "CodeBuild"
-      input_artifacts  = ["SourceOutput"]
-      output_artifacts = ["BuildOutput"]
-      run_order        = 1
-      configuration = {
-        ProjectName = aws_codebuild_project.codebuild.id
-      }
-    }
-  }
-
-  stage {
-    name = "Deploy"
-    action {
-      name            = "Deploy"
-      category        = "Deploy"
-      owner           = "AWS"
-      version         = "1"
-      provider        = "ECS"
-      run_order       = 1
-      input_artifacts = ["BuildOutput"]
-      configuration = {
-        ClusterName       = aws_ecs_cluster.fargate_cluster.name
-        ServiceName       = var.ecs_service_name
-        FileName          = "imagedefinitions.json"
-        DeploymentTimeout = "15"
-      }
-    }
-  }
-  //  https://gist.github.com/joestump/cac3abb94050186fcba1c57c8a880a71
-  //  https://github.com/cloudposse/terraform-aws-ecs-codepipeline/blob/master/main.tf#L314
-  lifecycle {
-    # prevent github OAuthToken from causing updates, since it's removed from state file
-    ignore_changes = [stage[0].action[0].configuration.OAuthToken]
-  }
-}
-
-resource "aws_codepipeline_webhook" "bar" {
-  name            = "${var.prefix}-webhook-github-bar"
-  authentication  = "GITHUB_HMAC"
-  target_action   = "Source"
-  target_pipeline = aws_codepipeline.pipeline.name
-
-  authentication_configuration {
-    secret_token = var.webhook_secret
-  }
-
-  filter {
-    json_path    = "$.ref"
-    match_equals = "refs/heads/{Branch}"
-  }
-}
-
-resource "github_repository_webhook" "bar" {
-  repository = var.repo_name
-
-  configuration {
-    url          = aws_codepipeline_webhook.bar.url
-    content_type = "json"
-    insecure_ssl = true
-    secret       = var.webhook_secret
-  }
-
-  events = ["push"]
-}
-
-output "pipeline_url" {
-  value = "https://console.aws.amazon.com/codepipeline/home?region=${data.aws_region.current.id}#/view/${aws_codepipeline.pipeline.id}"
-}
-
-variable "github_oauth_token" {
-}
-
-variable "repo_owner" {
-}
-
-variable "repo_name" {
-}
-
-variable "branch" {
-}
-
-variable "webhook_secret" {
-}
+////https://github.com/aws-samples/aws-ecs-cicd-terraform/blob/master/terraform/codepipeline.tf
+//provider "github" {}
+//
+//data "aws_iam_policy_document" "codepipeline_role_document" {
+//  version = "2012-10-17"
+//  statement {
+//    effect = "Allow"
+//    actions = ["sts:AssumeRole"]
+//    principals {
+//      type        = "Service"
+//      identifiers = ["codepipeline.amazonaws.com"]
+//    }
+//  }
+//}
+//
+//resource "aws_iam_role" "codepipeline_role" {
+//  name = "${var.prefix}-codepipeline_role"
+//  assume_role_policy = data.aws_iam_policy_document.codepipeline_role_document.json
+//}
+//
+//data "aws_iam_policy_document" "codepipeline_policy_document" {
+//  version = "2012-10-17"
+//  statement {
+//    actions = ["s3:GetObject", "s3:GetObjectVersion", "s3:PutObject",
+//    "s3:GetBucketVersioning"]
+//    effect    = "Allow"
+//    resources = ["${aws_s3_bucket.artifact_bucket.arn}/*"]
+//  }
+//  statement {
+//    actions = ["codebuild:StartBuild", "codebuild:BatchGetBuilds",
+//      "cloudformation:*",
+//    "iam:PassRole"]
+//    effect    = "Allow"
+//    resources = ["*"]
+//  }
+//  statement {
+//    actions   = ["ecs:*"]
+//    effect    = "Allow"
+//    resources = ["*"]
+//  }
+//}
+//
+//resource "aws_iam_policy" "codepipeline_policy" {
+//  description = "Policy to allow codepipeline to execute"
+//  policy      = data.aws_iam_policy_document.codepipeline_role_document.json
+//}
+//
+//resource "aws_iam_role_policy_attachment" "codepipeline-attach" {
+//  role       = aws_iam_role.codepipeline_role.name
+//  policy_arn = aws_iam_policy.codepipeline_policy.arn
+//}
+//
+//resource "aws_s3_bucket" "artifact_bucket" {
+//}
+//
+//resource "aws_codepipeline" "pipeline" {
+//  depends_on = [
+//    aws_codebuild_project.codebuild,
+//  ]
+//  name     = "${var.prefix}-Pipeline"
+//  role_arn = aws_iam_role.codepipeline_role.arn
+//  artifact_store {
+//    location = aws_s3_bucket.artifact_bucket.bucket
+//    type     = "S3"
+//  }
+//  //  https://docs.aws.amazon.com/code-samples/latest/catalog/cloudformation-codepipeline-template-codepipeline-github-events-yaml.yml.html
+//
+//  stage {
+//    name = "Source"
+//    action {
+//      name             = "Source"
+//      category         = "Source"
+//      owner            = "ThirdParty"
+//      version          = "1"
+//      provider         = "GitHub"
+//      output_artifacts = ["SourceOutput"] # code
+//      run_order        = 1
+//      configuration = {
+//        Owner                = var.repo_owner
+//        Repo                 = var.repo_name
+//        Branch               = var.branch
+//        OAuthToken           = var.github_oauth_token
+//        PollForSourceChanges = "false"
+//      }
+//    }
+//  }
+//
+//  stage {
+//    name = "Build"
+//    action {
+//      name             = "Build"
+//      category         = "Build"
+//      owner            = "AWS"
+//      version          = "1"
+//      provider         = "CodeBuild"
+//      input_artifacts  = ["SourceOutput"]
+//      output_artifacts = ["BuildOutput"]
+//      run_order        = 1
+//      configuration = {
+//        ProjectName = aws_codebuild_project.codebuild.id
+//      }
+//    }
+//  }
+//
+//  stage {
+//    name = "Deploy"
+//    action {
+//      name            = "Deploy"
+//      category        = "Deploy"
+//      owner           = "AWS"
+//      version         = "1"
+//      provider        = "ECS"
+//      run_order       = 1
+//      input_artifacts = ["BuildOutput"]
+//      configuration = {
+//        ClusterName       = aws_ecs_cluster.fargate_cluster.name
+//        ServiceName       = var.ecs_service_name
+//        FileName          = "imagedefinitions.json"
+//        DeploymentTimeout = "15"
+//      }
+//    }
+//  }
+//  //  https://gist.github.com/joestump/cac3abb94050186fcba1c57c8a880a71
+//  //  https://github.com/cloudposse/terraform-aws-ecs-codepipeline/blob/master/main.tf#L314
+//  lifecycle {
+//    # prevent github OAuthToken from causing updates, since it's removed from state file
+//    ignore_changes = [stage[0].action[0].configuration.OAuthToken]
+//  }
+//}
+//
+//resource "aws_codepipeline_webhook" "bar" {
+//  name            = "${var.prefix}-webhook-github-bar"
+//  authentication  = "GITHUB_HMAC"
+//  target_action   = "Source"
+//  target_pipeline = aws_codepipeline.pipeline.name
+//
+//  authentication_configuration {
+//    secret_token = var.webhook_secret
+//  }
+//
+//  filter {
+//    json_path    = "$.ref"
+//    match_equals = "refs/heads/{Branch}"
+//  }
+//}
+//
+//resource "github_repository_webhook" "bar" {
+//  repository = var.repo_name
+//
+//  configuration {
+//    url          = aws_codepipeline_webhook.bar.url
+//    content_type = "json"
+//    insecure_ssl = true
+//    secret       = var.webhook_secret
+//  }
+//
+//  events = ["push"]
+//}
+//
+//output "pipeline_url" {
+//  value = "https://console.aws.amazon.com/codepipeline/home?region=${data.aws_region.current.id}#/view/${aws_codepipeline.pipeline.id}"
+//}
+//
+//variable "github_oauth_token" {
+//}
+//
+//variable "repo_owner" {
+//}
+//
+//variable "repo_name" {
+//}
+//
+//variable "branch" {
+//}
+//
+//variable "webhook_secret" {
+//}

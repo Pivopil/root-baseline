91dd22263cae0e16da80fa3d8d88bb0ae9a4d39c
Pivopil <myworkplace8@gmail.com>
2021-03-14 21:07:24 +0200

    Created ECS and pipeline

--- codebuild.tf
+++ codebuild.tf
@@ -1,138 +1,138 @@
-////https://github.com/aws-samples/aws-ecs-cicd-terraform/blob/master/terraform/codebuild.tf
-//data "aws_iam_policy_document" "codebuild_role_document" {
-//  version = "2012-10-17"
-//  statement {
-//    effect = "Allow"
-//    actions = ["sts:AssumeRole"]
-//    principals {
-//      type        = "Service"
-//      identifiers = ["codebuild.amazonaws.com"]
-//    }
-//  }
-//}
-//
-//resource "aws_iam_role" "codebuild_role" {
-//  name = "${var.prefix}-codebuild_role"
-//  assume_role_policy = data.aws_iam_policy_document.codebuild_role_document.json
-//}
-//
-//data "aws_iam_policy_document" "codebuild_policy_document" {
-//  version = "2012-10-17"
-//  statement {
-//    actions   = ["logs:CreateLogGroup", "logs:CreateLogStream", "logs:PutLogEvents", "ecr:GetAuthorizationToken"]
-//    effect    = "Allow"
-//    resources = ["*"]
-//  }
-//  statement {
-//    actions   = ["s3:GetObject", "s3:GetObjectVersion", "s3:PutObject"]
-//    effect    = "Allow"
-//    resources = ["${aws_s3_bucket.artifact_bucket.arn}/*"]
-//  }
-//  statement {
-//    actions = ["ecr:GetDownloadUrlForLayer", "ecr:BatchGetImage",
-//      "ecr:BatchCheckLayerAvailability", "ecr:PutImage",
-//      "ecr:InitiateLayerUpload", "ecr:UploadLayerPart",
-//    "ecr:CompleteLayerUpload"]
-//    effect    = "Allow"
-//    resources = ["*"]
-//// todo: add specific resources like [aws_ecr_repository.springboot_ecr_repository.arn, arn:aws:ecr:REGION:ACCOUNT_ID:repository/openjdk]
-//  }
-//  statement {
-//    actions = ["ecr:GetDownloadUrlForLayer",
-//      "ecr:BatchGetImage",
-//    "ecr:BatchCheckLayerAvailability"]
-//    effect    = "Allow"
-//    resources = [aws_ecr_repository.springboot_ecr_repository.arn]
-//  }
-//}
-//
-//
-//resource "aws_iam_policy" "codebuild_policy" {
-//  description = "Policy to allow codebuild to execute build spec"
-//  policy      = data.aws_iam_policy_document.codebuild_policy_document.json
-//}
-//
-//resource "aws_iam_role_policy_attachment" "codebuild-attach" {
-//  role       = aws_iam_role.codebuild_role.name
-//  policy_arn = aws_iam_policy.codebuild_policy.arn
-//}
-//
-//resource "aws_codebuild_project" "codebuild" {
-//  depends_on = [
-//    aws_ecr_repository.springboot_ecr_repository
-//  ]
-//  name         = "${var.prefix}-codebuild"
-//  service_role = aws_iam_role.codebuild_role.arn
-//  artifacts {
-//    type = "CODEPIPELINE"
-//  }
-//  environment {
-//    compute_type                = "BUILD_GENERAL1_MEDIUM"
-//    image                       = "aws/codebuild/standard:3.0"
-//    type                        = "LINUX_CONTAINER"
-//    privileged_mode             = true
-//    image_pull_credentials_type = "CODEBUILD"
-//    environment_variable {
-//      name  = "REPOSITORY_URI"
-//      value = aws_ecr_repository.springboot_ecr_repository.repository_url
-//    }
-//    environment_variable {
-//      name  = "AWS_DEFAULT_REGION"
-//      value = data.aws_region.current.id
-//    }
-//    environment_variable {
-//      name  = "CONTAINER_NAME"
-//      value = var.ecs_service_name
-//    }
-//  }
-//  source {
-//    type      = "CODEPIPELINE"
-////    https://docs.aws.amazon.com/codebuild/latest/userguide/build-spec-ref.html#build-spec-ref-example
-//
-//    // todo: for build step use docker file to generate jar artifact instead of local mvn and java env
-//    buildspec = <<BUILDSPEC
-//version: 0.2
-//env:
-//  variables:
-//    CODEBUILD_SRC_DIR: "/ecs-app"
-//    SERVICE_NAME: "springbootapp"
-//runtime-versions:
-//  java: openjdk8
-//phases:
-//  install:
-//    runtime-versions:
-//      docker: 18
-//  pre_build:
-//    commands:
-//      - echo Logging in to Amazon ECR...
-//      - $(aws ecr get-login --region $AWS_DEFAULT_REGION --no-include-email)
-//      - COMMIT_HASH=$(echo $CODEBUILD_RESOLVED_SOURCE_VERSION | cut -c 1-7)
-//      - IMAGE_TAG=$${COMMIT_HASH:=latest}
-//  build:
-//    commands:
-//      - echo Build started on `date`
-//      - echo Building the jar
-//      - cd ./ecs-app
-//      - pwd
-//      - ls
-//      - mvn clean install
-//      - echo get target jar
-//      - find ./target/ -type f \( -name "*.jar" -not -name "*sources.jar" \) -exec cp {} ./springbootapp.jar \;
-//      - echo Building the Docker image...
-//      - pwd
-//      - ls
-//      - docker build -t $REPOSITORY_URI:latest .
-//      - docker tag $REPOSITORY_URI:latest $REPOSITORY_URI:$IMAGE_TAG
-//  post_build:
-//    commands:
-//      - echo Build completed on `date`
-//      - echo Pushing the Docker image...
-//      - docker push $REPOSITORY_URI:latest
-//      - docker push $REPOSITORY_URI:$IMAGE_TAG
-//      - cd ..
-//      - printf '[{"name":"%s","imageUri":"%s"}]' $CONTAINER_NAME $REPOSITORY_URI:$IMAGE_TAG > imagedefinitions.json
-//artifacts:
-//    files: imagedefinitions.json
-//BUILDSPEC
-//  }
-//}
+//https://github.com/aws-samples/aws-ecs-cicd-terraform/blob/master/terraform/codebuild.tf
+data "aws_iam_policy_document" "codebuild_role_document" {
+  version = "2012-10-17"
+  statement {
+    effect = "Allow"
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["codebuild.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "codebuild_role" {
+  name = "${var.prefix}-codebuild_role"
+  assume_role_policy = data.aws_iam_policy_document.codebuild_role_document.json
+}
+
+data "aws_iam_policy_document" "codebuild_policy_document" {
+  version = "2012-10-17"
+  statement {
+    actions   = ["logs:CreateLogGroup", "logs:CreateLogStream", "logs:PutLogEvents", "ecr:GetAuthorizationToken"]
+    effect    = "Allow"
+    resources = ["*"]
+  }
+  statement {
+    actions   = ["s3:GetObject", "s3:GetObjectVersion", "s3:PutObject"]
+    effect    = "Allow"
+    resources = ["${aws_s3_bucket.artifact_bucket.arn}/*"]
+  }
+  statement {
+    actions = ["ecr:GetDownloadUrlForLayer", "ecr:BatchGetImage",
+      "ecr:BatchCheckLayerAvailability", "ecr:PutImage",
+      "ecr:InitiateLayerUpload", "ecr:UploadLayerPart",
+    "ecr:CompleteLayerUpload"]
+    effect    = "Allow"
+    resources = ["*"]
+// todo: add specific resources like [aws_ecr_repository.springboot_ecr_repository.arn, arn:aws:ecr:REGION:ACCOUNT_ID:repository/openjdk]
+  }
+  statement {
+    actions = ["ecr:GetDownloadUrlForLayer",
+      "ecr:BatchGetImage",
+    "ecr:BatchCheckLayerAvailability"]
+    effect    = "Allow"
+    resources = [aws_ecr_repository.springboot_ecr_repository.arn]
+  }
+}
+
+
+resource "aws_iam_policy" "codebuild_policy" {
+  description = "Policy to allow codebuild to execute build spec"
+  policy      = data.aws_iam_policy_document.codebuild_policy_document.json
+}
+
+resource "aws_iam_role_policy_attachment" "codebuild-attach" {
+  role       = aws_iam_role.codebuild_role.name
+  policy_arn = aws_iam_policy.codebuild_policy.arn
+}
+
+resource "aws_codebuild_project" "codebuild" {
+  depends_on = [
+    aws_ecr_repository.springboot_ecr_repository
+  ]
+  name         = "${var.prefix}-codebuild"
+  service_role = aws_iam_role.codebuild_role.arn
+  artifacts {
+    type = "CODEPIPELINE"
+  }
+  environment {
+    compute_type                = "BUILD_GENERAL1_MEDIUM"
+    image                       = "aws/codebuild/standard:3.0"
+    type                        = "LINUX_CONTAINER"
+    privileged_mode             = true
+    image_pull_credentials_type = "CODEBUILD"
+    environment_variable {
+      name  = "REPOSITORY_URI"
+      value = aws_ecr_repository.springboot_ecr_repository.repository_url
+    }
+    environment_variable {
+      name  = "AWS_DEFAULT_REGION"
+      value = data.aws_region.current.id
+    }
+    environment_variable {
+      name  = "CONTAINER_NAME"
+      value = var.ecs_service_name
+    }
+  }
+  source {
+    type      = "CODEPIPELINE"
+//    https://docs.aws.amazon.com/codebuild/latest/userguide/build-spec-ref.html#build-spec-ref-example
+
+    // todo: for build step use docker file to generate jar artifact instead of local mvn and java env
+    buildspec = <<BUILDSPEC
+version: 0.2
+env:
+  variables:
+    CODEBUILD_SRC_DIR: "/ecs-app"
+    SERVICE_NAME: "springbootapp"
+runtime-versions:
+  java: openjdk8
+phases:
+  install:
+    runtime-versions:
+      docker: 18
+  pre_build:
+    commands:
+      - echo Logging in to Amazon ECR...
+      - $(aws ecr get-login --region $AWS_DEFAULT_REGION --no-include-email)
+      - COMMIT_HASH=$(echo $CODEBUILD_RESOLVED_SOURCE_VERSION | cut -c 1-7)
+      - IMAGE_TAG=$${COMMIT_HASH:=latest}
+  build:
+    commands:
+      - echo Build started on `date`
+      - echo Building the jar
+      - cd ./ecs-app
+      - pwd
+      - ls
+      - mvn clean install
+      - echo get target jar
+      - find ./target/ -type f \( -name "*.jar" -not -name "*sources.jar" \) -exec cp {} ./springbootapp.jar \;
+      - echo Building the Docker image...
+      - pwd
+      - ls
+      - docker build -t $REPOSITORY_URI:latest .
+      - docker tag $REPOSITORY_URI:latest $REPOSITORY_URI:$IMAGE_TAG
+  post_build:
+    commands:
+      - echo Build completed on `date`
+      - echo Pushing the Docker image...
+      - docker push $REPOSITORY_URI:latest
+      - docker push $REPOSITORY_URI:$IMAGE_TAG
+      - cd ..
+      - printf '[{"name":"%s","imageUri":"%s"}]' $CONTAINER_NAME $REPOSITORY_URI:$IMAGE_TAG > imagedefinitions.json
+artifacts:
+    files: imagedefinitions.json
+BUILDSPEC
+  }
+}
--- ecs.tf
+++ ecs.tf
@@ -1,112 +1,112 @@
-//resource "aws_ecs_cluster" "fargate_cluster" {
-//  name = local.aws_ecs_cluster_name
-//}
-//
-//locals {
-//  ecs_prefix           = "${var.prefix}-ecs"
-//  aws_ecs_cluster_name = "${var.prefix}-fargate-cluster"
-//}
-//
-//// https://docs.aws.amazon.com/elasticloadbalancing/latest/userguide/what-is-load-balancing.html
-//// https://www.terraform.io/docs/providers/aws/r/lb.html
-//resource "aws_alb" "ecs_cluster_alb" {
-//  name            = "${local.ecs_prefix}-alb"
-//  internal        = false
-//  security_groups = [aws_security_group.alb_sg.id]
-//  subnets         = tolist(data.aws_subnet_ids.default_subtets.ids)
-//
-//  access_logs {
-//    bucket  = aws_s3_bucket.alb_logs.bucket
-//    prefix  = "${var.prefix}-alb_logs"
-//    enabled = true
-//  }
-//}
-//
-////https://www.terraform.io/docs/providers/aws/r/lb_listener.html
-//resource "aws_alb_listener" "ecs_alb_https_listener" {
-//  load_balancer_arn = aws_alb.ecs_cluster_alb.arn
-//  port              = 443
-//  protocol          = "HTTPS"
-//  ssl_policy        = "ELBSecurityPolicy-TLS-1-2-2017-01"
-//  certificate_arn   = module.acm.this_acm_certificate_arn
-//
-//  default_action {
-//    type             = "forward"
-//    target_group_arn = aws_alb_target_group.ecs_default_target_group.arn
-//  }
-//
-//  depends_on = [aws_alb_target_group.ecs_default_target_group]
-//}
-//
-//// https://www.terraform.io/docs/providers/aws/r/lb_target_group.html
-//// https://docs.aws.amazon.com/elasticloadbalancing/latest/application/load-balancer-target-groups.html
-//resource "aws_alb_target_group" "ecs_default_target_group" {
-//  name       = "${local.ecs_prefix}-tg"
-//  port       = 80
-//  protocol   = "HTTP"
-//  vpc_id     = aws_default_vpc.default.id
-//  depends_on = [aws_alb.ecs_cluster_alb]
-//}
-//
-//// https://www.terraform.io/docs/providers/aws/r/route53_record.html
-//resource "aws_route53_record" "ecs_load_balancer_record" {
-//  name    = "*.ecs.${var.public_subdomain}"
-//  type    = "A"
-//  zone_id = data.aws_route53_zone.public.zone_id
-//
-//  alias {
-//    evaluate_target_health = false
-//    name                   = aws_alb.ecs_cluster_alb.dns_name
-//    zone_id                = aws_alb.ecs_cluster_alb.zone_id
-//  }
-//}
-//
-//resource "aws_iam_role" "ecs_cluster_role" {
-//  name               = "${local.ecs_prefix}-IAM-Role"
-//  assume_role_policy = <<EOF
-//{
-//"Version": "2012-10-17",
-//"Statement": [
-//  {
-//    "Effect": "Allow",
-//    "Principal": {
-//      "Service": ["ecs.amazonaws.com", "ec2.amazonaws.com", "application-autoscaling.amazonaws.com"]
-//    },
-//    "Action": "sts:AssumeRole"
-//  }
-//  ]
-//}
-//EOF
-//
-//}
-//
-//resource "aws_iam_role_policy" "ecs_cluster_policy" {
-//  name   = "${local.ecs_prefix}-IAM-Policy"
-//  role   = aws_iam_role.ecs_cluster_role.id
-//  policy = <<EOF
-//{
-//  "Version": "2012-10-17",
-//  "Statement": [
-//    {
-//      "Effect": "Allow",
-//      "Action": [
-//        "ecs:*",
-//        "ec2:*",
-//        "elasticloadbalancing:*",
-//        "ecr:*",
-//        "dynamodb:*",
-//        "cloudwatch:*",
-//        "s3:*",
-//        "rds:*",
-//        "sqs:*",
-//        "sns:*",
-//        "logs:*",
-//        "ssm:*"
-//      ],
-//      "Resource": "*"
-//    }
-//  ]
-//}
-//EOF
-//
-//}
+resource "aws_ecs_cluster" "fargate_cluster" {
+  name = local.aws_ecs_cluster_name
+}
+
+locals {
+  ecs_prefix           = "${var.prefix}-ecs"
+  aws_ecs_cluster_name = "${var.prefix}-fargate-cluster"
+}
+
+// https://docs.aws.amazon.com/elasticloadbalancing/latest/userguide/what-is-load-balancing.html
+// https://www.terraform.io/docs/providers/aws/r/lb.html
+resource "aws_alb" "ecs_cluster_alb" {
+  name            = "${local.ecs_prefix}-alb"
+  internal        = false
+  security_groups = [aws_security_group.alb_sg.id]
+  subnets         = tolist(data.aws_subnet_ids.default_subtets.ids)
+
+  access_logs {
+    bucket  = aws_s3_bucket.alb_logs.bucket
+    prefix  = "${var.prefix}-alb_logs"
+    enabled = true
+  }
+}
+
+//https://www.terraform.io/docs/providers/aws/r/lb_listener.html
+resource "aws_alb_listener" "ecs_alb_https_listener" {
+  load_balancer_arn = aws_alb.ecs_cluster_alb.arn
+  port              = 443
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-TLS-1-2-2017-01"
+  certificate_arn   = module.acm.this_acm_certificate_arn
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_alb_target_group.ecs_default_target_group.arn
+  }
+
+  depends_on = [aws_alb_target_group.ecs_default_target_group]
+}
+
+// https://www.terraform.io/docs/providers/aws/r/lb_target_group.html
+// https://docs.aws.amazon.com/elasticloadbalancing/latest/application/load-balancer-target-groups.html
+resource "aws_alb_target_group" "ecs_default_target_group" {
+  name       = "${local.ecs_prefix}-tg"
+  port       = 80
+  protocol   = "HTTP"
+  vpc_id     = aws_default_vpc.default.id
+  depends_on = [aws_alb.ecs_cluster_alb]
+}
+
+// https://www.terraform.io/docs/providers/aws/r/route53_record.html
+resource "aws_route53_record" "ecs_load_balancer_record" {
+  name    = "*.ecs.${var.public_subdomain}"
+  type    = "A"
+  zone_id = data.aws_route53_zone.public.zone_id
+
+  alias {
+    evaluate_target_health = false
+    name                   = aws_alb.ecs_cluster_alb.dns_name
+    zone_id                = aws_alb.ecs_cluster_alb.zone_id
+  }
+}
+
+resource "aws_iam_role" "ecs_cluster_role" {
+  name               = "${local.ecs_prefix}-IAM-Role"
+  assume_role_policy = <<EOF
+{
+"Version": "2012-10-17",
+"Statement": [
+  {
+    "Effect": "Allow",
+    "Principal": {
+      "Service": ["ecs.amazonaws.com", "ec2.amazonaws.com", "application-autoscaling.amazonaws.com"]
+    },
+    "Action": "sts:AssumeRole"
+  }
+  ]
+}
+EOF
+
+}
+
+resource "aws_iam_role_policy" "ecs_cluster_policy" {
+  name   = "${local.ecs_prefix}-IAM-Policy"
+  role   = aws_iam_role.ecs_cluster_role.id
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ecs:*",
+        "ec2:*",
+        "elasticloadbalancing:*",
+        "ecr:*",
+        "dynamodb:*",
+        "cloudwatch:*",
+        "s3:*",
+        "rds:*",
+        "sqs:*",
+        "sns:*",
+        "logs:*",
+        "ssm:*"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+
+}

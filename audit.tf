8986bb8a503687b950b97890e261875a03a2aea4
Pivopil <myworkplace8@gmail.com>
2021-03-17 20:29:39 +0200

    Added audit logic

---
+++ audit.tf
@@ -0,0 +1,100 @@
+locals {
+  add_subscription_function_name = "${var.prefix}-AddSubscriptionLambda"
+  add_subscription_function_source_path         = "${path.module}/lambda-log-centralizer/handlers"
+}
+
+data "archive_file" "AddSubscriptionLambdaSourceCode" {
+  type        = "zip"
+  source_dir  = local.add_subscription_function_source_path
+  output_path = "${local.add_subscription_function_source_path}.zip"
+}
+
+data "aws_iam_policy_document" "AddSubscriptionFilterRole_sts_policy" {
+  version = "2012-10-17"
+  statement {
+    principals {
+      identifiers = ["lambda.amazonaws.com"]
+      type        = "Service"
+    }
+    effect = "Allow"
+    actions = [
+      "sts:AssumeRole"
+    ]
+  }
+}
+
+resource "aws_iam_role" "AddSubscriptionFilterRole" {
+  name = "${var.prefix}-AddSubscriptionFilterRole"
+  assume_role_policy = data.aws_iam_policy_document.AddSubscriptionFilterRole_sts_policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "AddSubscriptionFilterRole_policies" {
+  for_each = toset([
+    "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+  ])
+
+  policy_arn = each.key
+  role       = aws_iam_role.AddSubscriptionFilterRole.name
+}
+
+data "aws_iam_policy_document" "AddSubcriptionPolicy_policy_document" {
+  version = "2012-10-17"
+  statement {
+    effect    = "Allow"
+    actions   = ["logs:*", "ssm:GetParameter"]
+    resources = ["*"]
+  }
+}
+
+resource "aws_cloudwatch_log_group" "add_subscription_function_log_group" {
+  name              = "/aws/lambda/${local.add_subscription_function_name}"
+  retention_in_days = 1
+}
+
+resource "aws_iam_role_policy" "AddSubscriptionLambda_execution_policy" {
+  name   = "${var.prefix}-AddSubscriptionLambdaPolicy"
+  role   = aws_iam_role.AddSubscriptionFilterRole.id
+  policy = data.aws_iam_policy_document.AddSubcriptionPolicy_policy_document.json
+}
+
+resource "aws_cloudwatch_event_rule" "CreateLogGroupEvent" {
+  event_pattern = jsonencode({
+    "source":["aws.logs"],
+    "detail-type":["AWS API Call via CloudTrail"],
+    "detail":{"eventSource":["logs.amazonaws.com"],
+      "eventName":["CreateLogGroup"]
+    }})
+  name = "${var.prefix}-CreateLogGroupEvent"
+  is_enabled = true
+}
+
+resource "aws_cloudwatch_event_target" "CreateLogGroupEvent_target" {
+  target_id = "add_new_subscription"
+  arn = aws_lambda_function.AddSubscriptionLambda.arn
+  rule = aws_cloudwatch_event_rule.CreateLogGroupEvent.name
+}
+
+resource "aws_lambda_permission" "AddSubscriptionLambdaPermission" {
+  action = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.AddSubscriptionLambda.arn
+  principal = "events.amazonaws.com"
+  source_arn = aws_cloudwatch_event_rule.CreateLogGroupEvent.arn
+}
+
+variable "audit_account_id" {
+}
+
+resource "aws_lambda_function" "AddSubscriptionLambda" {
+  function_name    = local.add_subscription_function_name
+  role             = aws_iam_role.AddSubscriptionFilterRole.arn
+  runtime          = "python3.7"
+  timeout          = 30
+  handler          = "lambda.lambda_handler"
+  filename         = "${local.add_subscription_function_source_path}.zip"
+  source_code_hash = data.archive_file.AddSubscriptionLambdaSourceCode.output_base64sha256
+  environment {
+    variables {
+      audit_destination_arn = "arn:aws:logs:us-east-1:${var.audit_account_id}:destination:CentralLogDestination"
+    }
+  }
+}
---
+++ lambda-log-centralizer/handlers/CentralLogging.py
@@ -0,0 +1,76 @@
+# https://github.com/aws-samples/amazon-cloudwatch-log-centralizer/blob/master/python/CentralLogging.py
+
+import boto3
+
+
+class CentralLogging:
+    def __init__(self):
+        self.log_client = boto3.client('logs')
+#         self.ssm_client = boto3.client('ssm')
+        print('Central Logger Made')
+
+    def add_subscriptions_to_existing_log_groups(self):
+
+        # Retrieve all of the existing log groups
+        log_group_response = self.log_client.describe_log_groups()
+
+        # Loop over multiple calls to describe_log_groups() as necessary using the next token
+        while True:
+            # If there are log groups, iterate over each one, retrieve its name, and call code to add the subscription to it
+            if log_group_response:
+
+                for log_group in log_group_response['logGroups']:
+                    log_group_name = log_group['logGroupName']
+                    self.add_subscription_filter(log_group_name)
+
+                if 'nextToken' in log_group_response:
+                    log_groups_next_token = log_group_response['nextToken']
+
+                    if log_groups_next_token:
+                        log_group_response = self.log_client.describe_log_groups(nextToken=log_groups_next_token)
+                    else:
+                        break
+
+                else:
+                    break
+
+    # Add subscription to centralized logging to the log group with log_group_name
+    def add_subscription_filter(self, log_group_name):
+#         # Retrieve the destination for the subscription from the Parameter Store
+#         destination_response = self.ssm_client.get_parameter(Name='LogDestination')
+#
+#         # Error if no destination, otherwise extract destination id from response
+#         if not destination_response:
+#             raise ValueError(
+#                 'Cannot locate central logging destination, put_subscription_filter failed')
+#         else:
+#             destination = destination_response['Parameter']['Value']
+#
+#         # Error to try to add subscription if one already exists, so delete any existing subscription from this log group
+#         self.delete_existing_subscription_filter(log_group_name)
+        print(">>>>>>>>> audit_destination_arn <<<<<<<<<<<")
+        print(os.environ['audit_destination_arn'])
+        # Put the new subscription with the destination onto the log group
+        self.log_client.put_subscription_filter(
+            logGroupName=log_group_name,
+            filterName='Destination',
+            filterPattern='',
+            destinationArn=os.environ['audit_destination_arn']
+        )
+
+    # Delete an existing subscription from the log group
+    def delete_existing_subscription_filter(self, log_group_name):
+        # Retrieve any existing subscription filters (only can be one)
+        subscription_filters = self.log_client.describe_subscription_filters(
+            logGroupName=log_group_name)
+
+        # Iterate over results if there are any (again, should not be multiple, but to follow the convention of the SDK)
+        for subscription_filter in subscription_filters['subscriptionFilters']:
+            # Retrieve the subscription filter name to use in the call to delete
+            filter_name = subscription_filter['filterName']
+
+            # Delete any subscriptions that are found on the log group
+            self.log_client.delete_subscription_filter(
+                logGroupName=log_group_name,
+                filterName=filter_name
+            )

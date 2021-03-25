b4f98334768e7a29bad2827049b2004292747a00
Pivopil <myworkplace8@gmail.com>
2021-03-25 10:49:25 +0200

    Created EC2

--- tags.tf
+++ tags.tf
@@ -1,3 +1,8 @@
+locals {
+  tag_region = data.aws_region.current.name
+  tag_aws_account_id = data.aws_caller_identity.current.account_id
+}
+
 
 module "custom_tags" {
   // git::https://github.com/Pivopil/awsdevbot-app.git//modules/tags?ref=main
@@ -9,19 +14,19 @@ module "custom_tags" {
   app_env_component_name    = var.public_subdomain
   app_env_component_version = "0.0.1"
   support_email             = "example@suport.com"
-  owner                     = "arn:aws:sts::${data.aws_caller_identity.current.account_id}:assumed-role/OrganizationAccountAccessRole"
+  owner                     = "arn:aws:sts::${local.tag_aws_account_id}:assumed-role/OrganizationAccountAccessRole"
   is_temp                   = true
   expires_at                = timeadd("2021-11-22T00:00:00Z", "720h")
   deploy_tool               = "terraform 14.5"
   vcs                       = "github.com/Pivopil/root-baseline"
   backup_enabled            = false
-  aws_account_id            = data.aws_caller_identity.current.account_id
+  aws_account_id            = local.tag_aws_account_id
   company                   = var.public_subdomain
   compliance                = "GDPR"
   data_sensitivity          = "Public"
   encryption                = true
   public_facing             = true
-  region                    = data.aws_region.current.name
+  region                    = local.tag_region
   backup_config             = "no-backup"
 }
 
